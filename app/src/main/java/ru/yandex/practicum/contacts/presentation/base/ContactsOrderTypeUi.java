d548c2dcb642d0a618b3efc619e2abbfe7e631cc
SergeyNoskov2022 <wraith10@yandex.ru>
2022-11-18 00:25:33 +0500

    end practic

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import androidx.annotation.NonNull;
+
+import ru.yandex.practicum.contacts.presentation.sort.SortTypeUI;
+
+public abstract class ContactsOrderTypeUi {
+
+    protected final String type;
+    protected final boolean isSelected;
+
+    public ContactsOrderTypeUi(@NonNull String type, boolean isSelected) {
+        this.type = type;
+        this.isSelected = isSelected;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+
+    public abstract String createLogMessage(); //{        return "Выбран тип сортировки: " + type;    }
+
+}
+
